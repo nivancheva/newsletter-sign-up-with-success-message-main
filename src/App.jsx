66886eede92f81b60fb8aba8f6d56a6dedb1c96a
nivancheva <nedyalka.tsintsarova@gmail.com>
2023-10-03 13:31:56 +0300

    add basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
 import './App.css'
+import iconList from './images/icon-list.svg'
+
 
 function App() {
   return (
-    <>
-      Test
-    </>
+    <div className='toRemove'>
+      <div><button className='button'>Test</button></div>
+      <ul className='list'>
+        <li className='list-item'>
+          <img src={iconList}/>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, suscipit?</p>
+        </li>
+        <li className='list-item'>
+          <img src={iconList}/>
+          <p>Testkjsnhasj</p>
+        </li>
+        <li className='list-item'>
+          <img src={iconList}/>
+          <p>Testkjsnhasj</p>
+        </li>
+      </ul>
+
+      <label className='label' htmlFor='email'>Email address</label>
+      <div><input 
+        className='input'
+        id='email'
+        name='email'
+        type='email'
+        placeholder='email@company.com'
+      /></div>
+    </div>
   )
 }
 
